7bfae73d29ffb7fac988dc2acbd5c8c4a60e0497
syedmoiz05 <147015033+syedmoiz05@users.noreply.github.com>
2025-02-13 09:03:36 -0800

    Delete Printable.java

--- Printable.java
+++
@@ -1,6 +0,0 @@
-/* The Printable interface declares method to print a summary
-   Any class using this interface has to have a printSummary() method.
-*/
-public interface Printable {
-    void printSummary();
-}
