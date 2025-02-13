c534284a24cc66431bcf3f9736680559c73dca42
syedmoiz05 <147015033+syedmoiz05@users.noreply.github.com>
2025-02-13 08:56:33 -0800

    Add files via upload

---
+++ Project/Printable.java
@@ -0,0 +1,6 @@
+/* The Printable interface declares method to print a summary
+   Any class using this interface has to have a printSummary() method.
+*/
+public interface Printable {
+    void printSummary();
+}
---
+++ Project/Schedulable.java
@@ -0,0 +1,6 @@
+/* Schedulable interface declares a method to schedule an appointment
+   Any class using this must have a schedule() method.
+*/
+public interface Schedulable {
+    void schedule();
+}
