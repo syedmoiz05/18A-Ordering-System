f45c2d3e46103cefb6a2d2ccdf3bab81b332ec4a
syedmoiz05 <147015033+syedmoiz05@users.noreply.github.com>
2025-02-13 08:57:48 -0800

    Delete Schedulable.java

--- Schedulable.java
+++
@@ -1,6 +0,0 @@
-/* Schedulable interface declares a method to schedule an appointment
-   Any class using this must have a schedule() method.
-*/
-public interface Schedulable {
-    void schedule();
-}
